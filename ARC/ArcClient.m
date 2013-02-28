b7e96702e812e3ed6c952732d96357d590a1138d
Nick Wroblewski <njw438@gmail.com>
2013-02-28 13:49:11 -0600

    remove logs

--- ARC/ArcClient.m
+++ ARC/ArcClient.m
@@ -71,7 +71,7 @@ NSString *const ARC_ERROR_MSG = @"Arc Error, try again later";
         if ([prefs valueForKey:@"arcUrl"] && ([[prefs valueForKey:@"arcUrl"] length] > 0)) {
            _arcUrl = [prefs valueForKey:@"arcUrl"];
         }
-        NSLog(@"***** Arc URL = %@ *****", _arcUrl);
+        //NSLog(@"***** Arc URL = %@ *****", _arcUrl);
     }
     return self;
 }
@@ -641,7 +641,7 @@ NSString *const ARC_ERROR_MSG = @"Arc Error, try again later";
         NSData *returnData = [NSData dataWithData:self.serverData];
         NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
         
-        NSLog(@"ReturnString: %@", returnString);
+        //NSLog(@"ReturnString: %@", returnString);
         
         SBJsonParser *jsonParser = [SBJsonParser new];
         NSDictionary *response = (NSDictionary *) [jsonParser objectWithString:returnString error:NULL];
